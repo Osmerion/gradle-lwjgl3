7eb46b560bbcd3fa0d59c4ece05abbb0bd2c2c25
Leon Linhart <themrmilchmann@gmail.com>
2023-09-08 15:25:36 +0200

    build: bump version number {0.1.0 => 0.2.0}

--- build-logic/src/main/kotlin/com.osmerion.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.osmerion.base-conventions.gradle.kts
@@ -33,7 +33,7 @@ import com.osmerion.build.BuildType
 
 group = "com.osmerion.gradle.lwjgl3"
 
-val nextVersion = "0.1.0"
+val nextVersion = "0.2.0"
 version = when (deployment.type) {
     BuildType.SNAPSHOT -> "$nextVersion-SNAPSHOT"
     else -> nextVersion
--- docs/changelog/README.md
+++ docs/changelog/README.md
@@ -4,6 +4,7 @@
 
 ### Version History
 
+* ????-??-?? [0.2.0](0.2.0.md) (TBD; work in progress)
 * 2023-07-03 [0.1.0](0.1.0.md) (latest)
 
 
