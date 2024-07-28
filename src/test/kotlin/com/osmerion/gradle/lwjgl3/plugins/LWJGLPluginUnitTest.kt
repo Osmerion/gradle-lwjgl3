fd3fd2a361aa283aad38858dc358d24d74759ea5
Leon Linhart <themrmilchmann@gmail.com>
2024-07-28 21:58:24 +0200

    chore: update copyright headers

--- src/test/kotlin/com/osmerion/gradle/lwjgl3/plugins/LWJGLPluginUnitTest.kt
+++ src/test/kotlin/com/osmerion/gradle/lwjgl3/plugins/LWJGLPluginUnitTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2023 Leon Linhart,
+ * Copyright (c) 2021-2024 Leon Linhart,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
